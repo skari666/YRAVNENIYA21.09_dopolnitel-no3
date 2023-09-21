89f79d06c4340d5d79df55e80f0db4e3712ed2fd
skari666 <144269531+skari666@users.noreply.github.com>
2023-09-21 20:09:34 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение для альфа:");
+        string alphaInput = Console.ReadLine();
+
+        if (!double.TryParse(alphaInput, out double alpha))
+        {
+            Console.WriteLine("Неправильный формат ввода.");
+            return;
+        }
+
+        if (alpha == 0)
+        {
+            Console.WriteLine("Значение альфа не может быть равным нулю.");
+            return;
+        }
+
+        double result = Math.Cos(alpha) + Math.Sin(alpha) + Math.Pow(Math.Cos(alpha), 2) + Math.Pow(Math.Sin(alpha), 3);
+
+        Console.WriteLine($"Значение Y = {result}");
+        Console.ReadKey();
+    }
+}
+
